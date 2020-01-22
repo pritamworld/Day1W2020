3372e2d12164e1ec731572a28d1a3703b69b5eaf
moxDroid Labs Inc <pritam.world@gmail.com>
2020-01-21 20:48:51 -0500

    [PNP] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,19 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to JAVA Programming");
+
+        for(int i = 1; i <= 10; i++)
+            System.out.println("Pritesh Patel");
+
+        int x = 100;
+        int y = 800;
+        int c = 200;
+         
+    }
+}
